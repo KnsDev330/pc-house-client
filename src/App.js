3355a9936ab1449853f5e91552b2ea0b437e9556
Khandaker Sajjat <me@kns.dev>
2022-05-24 22:41:24 +0600

    added dashboard link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Login from './Components/Shared/Login/Login';
 import Register from './Components/Shared/Register/Register';
 import 'react-toastify/dist/ReactToastify.css';
 import ResetPassword from './Components/Shared/ResetPassword/ResetPassword';
+import Reviews from './Components/Shared/Reviews/Reviews';
+import Dashboard from './Components/Shared/Dashboard/Dashboard';
 
 function App() {
     return (
@@ -16,10 +18,15 @@ function App() {
 
             <Header />
             <Routes>
-                <Route path='/' element={<Home />}></Route>
-                <Route path='/login' element={<Login />}></Route>
-                <Route path='/register' element={<Register />}></Route>
-                <Route path='/reset-password' element={<ResetPassword />}></Route>
+                <Route path='' element={<Home />}></Route>
+                <Route path='login' element={<Login />}></Route>
+                <Route path='register' element={<Register />}></Route>
+                <Route path='reset-password' element={<ResetPassword />}></Route>
+                <Route path='reviews' element={<Reviews />}></Route>
+                <Route path='dashboard'>
+                    <Route index element={<Dashboard />} />
+                    <Route path='profile' element={<Footer />} />
+                </Route>
             </Routes>
             <Footer />
 
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -16,24 +16,26 @@ const Header = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link to='/home'>Home</Link></li>
                         <li><Link to='/reviews'>Reviews</Link></li>
                         <li><Link to='/items'>All Items</Link></li>
                         <li><Link to='/blogs'>Blogs</Link></li>
+                        {user && <li><Link to='/dashboard' className='font-semibold'>Dashboard</Link></li>}
                     </ul>
                 </div>
                 <Link to='/' className="btn btn-ghost normal-case text-xl">PC-HOUSE</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
+                    <li><Link to='/home'>Home</Link></li>
                     <li><Link to='/reviews'>Reviews</Link></li>
                     <li><Link to='/items'>All Items</Link></li>
                     <li><Link to='/blogs'>Blogs</Link></li>
+                    {user && <li><Link to='/dashboard' className='font-semibold'>Dashboard</Link></li>}
                 </ul>
             </div>
             <div className="navbar-end pr-8">
-                {
-                    user ? <button className="btn btn-outline btn-error" onClick={() => signOut(auth)}>Logout</button> : <Link to='/login' className="btn">Login</Link>
-                }
+                {user ? <button className="btn btn-outline btn-error" onClick={() => signOut(auth)}>Logout</button> : <Link to='/login' className="btn">Login</Link>}
             </div>
         </div>
     );
